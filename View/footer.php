3c3b623ffaec6c2a69e1f5be4acc3693dcdb7d04
LucasPeresFaria <48368808+LucasPeresFaria@users.noreply.github.com>
2019-08-21 14:29:46 -0300

    footer (não finalizado)

--- View/footer.php
+++ View/footer.php
@@ -7,10 +7,27 @@
 
 				<!-- Menu -->
 					
-					<!-- Rodapé da barra lateral -->
-					<footer id="footer">
-						<p>Testando rodapé</p>
-					</footer>
+					<!-- Footer -->
+                    <footer class="page-footer font-small cyan darken-3">
+
+                        <!-- Footer Elements -->
+                        <div class="container">
+                        <!-- Grid row-->
+                        <div class="row">
+
+                            <!-- Grid column -->
+                            <div class="col-md-12 py-5">
+                            <div class="mb-5 flex-center">
+
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+                        <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+                        </div>
+                    </footer>
 					
 					<!-- Scripts -->
                     <script type="text/javascript" language="javascript" src="../JS/jquery-3.2.1.js"></script>
