db4941d6a2b9472d8e0f65e0694a16c538e29af3
LucasPeresFaria <48368808+LucasPeresFaria@users.noreply.github.com>
2019-10-03 10:25:51 -0300

    Update candidato_cadastrar.php

--- View/candidato_cadastrar.php
+++ View/candidato_cadastrar.php
@@ -31,10 +31,9 @@ if (isset($_POST['btnCadastrar'])) {
   <?php include_once 'header.php'; ?>
 
             <header class="masthead text-white text-center" id="home">
-               <div class="container">
                    <div class="row">
                 
-                        <form action="candidato_cadastrar.php" method="post">
+                        <!-- <form action="candidato_cadastrar.php" method="post">
 
                             <div class=form-row>
                                 <div class="form-group col">
@@ -71,11 +70,28 @@ if (isset($_POST['btnCadastrar'])) {
                                     <input type="submit" id="btnCadastrar" name="btnCadastrar" class="btn btn-warning btn-lg" value="Cadastrar!">
                                 </div>
                             </div>
+                        </form>  -->
 
-                        </form>         
+
+                        <form class="form-signin mx-5" action="candidato_cadastrar.php" method="post">
+                            <h1 class="h3 mb-3 font-weight-normal">Faça seu cadastro</h1>
+
+                            <label for="txtCpf" class="sr-only">CPF</label>
+                            <input id="txtCpf" name="txtCpf" class="form-control mb-1" placeholder="Insira seu CPF.." autofocus="" required>
+                            
+                            <label for="txtEmail" class="sr-only">Endereço de email</label>
+                            <input type="email" id="txtEmail" name="txtEmail" class="form-control mb-1" placeholder="Insira seu e-mail..." required>
+                            
+                            <label for="txtSenha" class="sr-only">Senha</label>
+                            <input type="password" id="txtSenha" name="txtSenha" class="form-control mb-1" placeholder="Insira uma senha..." required>
+                            
+                            <label for="txtRepetirSenha" class="sr-only">Repetir Senha</label>   
+                            <input type="password" id="txtRepetirSenha" name="txtRepetirSenha" class="form-control mb-3" placeholder="Repita a senha..." required>
+                        
+                                <input type="submit" id="btnCadastrar" name="btnCadastrar" class="btn btn-warning btn-lg" value="Cadastrar!">
+                        </form>       
                       
                    </div>
-               </div>
           </header>
   
 <?php include 'footer.php'; ?>
