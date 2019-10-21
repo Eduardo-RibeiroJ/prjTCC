14f65f0005456d0e9cc9b8e8358491ddbf656a23
Eduardo-RibeiroJ <49720704+Eduardo-RibeiroJ@users.noreply.github.com>
2019-10-20 22:36:24 -0300

    Acertando botoes da tecla do recrutador

--- View/recrutador.php
+++ View/recrutador.php
@@ -5,121 +5,57 @@
 
 <?php include_once 'headerRecrut.php'; ?>
 
+<section class="masthead" id="sectionRecrutador" style="background: url(imagem/90463.jpg); background-size: cover;">
+  <div class="container">
 
-<div class="container">
-
-  <div class="jumbotron p-3 p-md-5">
-    <div class="container p-0">
-      <h5 class="display-4 display-md-2">Seja Bem-Vindo!</h1>
-      
-      <hr class="my-2 my-md-4">
-      <p class="lead">Gerencie aqui todas as atividades do processo seletivo.</p>
+    <div class="jumbotron p-3 p-md-5" style="background-color: #FFF">
+      <div class="container p-0">
+        <h5 class="display-4 display-md-2">Aproveite a melhor ferramenta para encontrar o candidato ideal!</h1>
+        
+        <hr class="my-2 my-md-4">
+        <p class="lead">Acompanhe e gerencie aqui os seus processos seletivos.</p>
+      </div>
     </div>
-  </div>
 
-  <section id="sectionRecrutador">
 
-      <div class="row">
 
-        <div class="col-md-4">
-          <div class="btn-grande">
-            <div class="row">
+        <div class="row">
 
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-folder-open fa-3x"></i>
-              </div>
+          <div class="col-md-6 text-center">
+            <button class="btn btn-primary btn-grande"><i class="fas fa-plus fa-3x"> </i> <h4>Abrir novo processo seletivo</h4></button>
+          </div>
 
-              <div class="col-md-9 text-center">
-                <h4>Processos seletivos em andamento</h4>
-              </div>
-            </div>
+          <div class="col-md-6">
+            <button class="btn btn-primary btn-grande"><i class="fas fa-folder-open fa-3x"> </i> <h4>Processos seletivos em andamento</h4></button>
+          </div>
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-6 text-center">
+            <button class="btn btn-primary btn-grande"><i class="fas fa-tasks fa-3x"> </i> <h4>Gerenciar testes online</h4></button>
+          </div>
+
+          <div class="col-md-6">
+            <button class="btn btn-primary btn-grande"><i class="far fa-comment-dots fa-3x"> </i> <h4>Gerenciar perguntas</h4></button>
           </div>
         </div>
 
-        <div class="col-md-4">
-          <div class="btn-grande">
+        <div class="row">
 
-            <div class="row">
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-archive fa-3x"></i>
-              </div>
-
-              <div class="col-md-9 text-center">
-                <h4>Processos seletivos encerrados</h4>
-              </div>
-            </div>
+          <div class="col-md-6">
+            <button class="btn btn-primary btn-grande"><i class="fas fa-user-tie fa-3x"> </i> <h4>Pesquisar candidatos</h4></button>
           </div>
+
+          <div class="col-md-6">
+            <button class="btn btn-primary btn-grande"><i class="fas fa-archive fa-3x"> </i> <h4>Processos seletivos encerrados</h4></button>
+          </div>
+
         </div>
 
-        <div class="col-md-4">
-          <div class="btn-grande">
-            <div class="row">
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-plus fa-3x"></i>
-              </div>
-
-              <div class="col-md-9 text-center">
-                <h4>Abrir novo processo seletivo</h4>
-              </div>
-            </div>
-          </div>  
-        </div> 
-      </div>
-
-      <div class="row">
-
-        <div class="col-md-6">
-          <div class="btn-grande">
-            <div class="row">
-
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-folder-open fa-3x"></i>
-              </div>
-
-              <div class="col-md-9 text-center">
-                <h4>Processos seletivos em andamento</h4>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-6">
-          <div class="btn-grande">
-
-            <div class="row">
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-archive fa-3x"></i>
-              </div>
-
-              <div class="col-md-9 text-center">
-                <h4>Processos seletivos encerrados</h4>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-
-        <div class="col">
-          <div class="btn-grande">
-            <div class="row">
-
-              <div class="col-md-3 text-center my-auto">
-                <i class="fas fa-folder-open fa-3x float-right"></i>
-              </div>
-
-              <div class="col-md-9 text-center">
-                <h4>Processos seletivos em andamento</h4>
-              </div>
-            </div>
-          </div>
-        </div> 
-      </div>
-
-    </section>
-  </div>
-
+    </div>
+  </section>
 
 
 
